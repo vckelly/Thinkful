6f60bea3ed1fb1b00ad762939e18fd7c137fefd2
vckelly <vckelly@gmail.com>
2014-11-12 02:02:10 +0000

    PB project

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,21 +1,18 @@
-fizz = range(1, 100)
+fizz = range(1, 101)
 buzz = []
 
 for i in fizz: 
 
-	if (i % 5) == 0 and (i % 3) == 0:
-		buzz.append("fizzbuzz")
-
-	elif (i % 5) == 0: 
-		buzz.append("buzz")
-
-	elif (i % 3) == 0:
-		buzz.append("fizz")
-
-	else: 
-		buzz.append(i)
+  if not (i % 3) == 0:
+		buzz += "fizz"
+    
+	if not (i % 5) == 0:
+		buzz += "buzz"
+  
+ # else: 
+		#buzz.append(i)
 		
 print "Fizz buzz counting up to 100" 
 
-print buzz
+print buzz or str(i)
 
---
+++ pb.py
@@ -0,0 +1,48 @@
+import random
+
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?"
+}
+
+ingredients = {
+    "strong": ["glug of rum", "slug of whisky", "splash of gin"],
+    "salty": ["olive on a stick", "salt-dusted rim", "rasher of bacon"],
+    "bitter": ["shake of bitters", "splash of tonic", "twist of lemon peel"],
+    "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
+    "fruity": ["slice of orange", "dash of cassis", "cherry on top"]
+}
+
+def get_pref():
+  preferences = {}
+  
+  for pref, question in questions.iteritems():
+    print question
+    preferences[pref] = raw_input().lower() in ['y', 'yes']
+    print ""
+    
+  return preferences
+
+def build_drink(preferences):
+  drink = []
+  for ingredient_kind, liked in preferences.iteritems():
+    if not liked:
+      continue
+      
+    drink.append(random.choice(ingredients[ingredient_kind]))
+  return drink
+
+def main():
+	preferences = get_pref()
+	drink = build_drink(preferences)
+	print "Here be yer drink, Sailor!"
+	print "This here is the concoction:"
+	for ingredient in drink:
+		print "A {}".format(ingredient)
+
+if __name__ == "__main__":
+	main()
+  
