ce153b12492d7bd477e74596991a2e173c254877
vckelly <vckelly@gmail.com>
2014-11-14 01:19:11 +0000

    fb edit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,12 +5,10 @@ for i in fizz:
 
   if not (i % 3) == 0:
 		buzz += "fizz"
-    
+
 	if not (i % 5) == 0:
 		buzz += "buzz"
   
- # else: 
-		#buzz.append(i)
 		
 print "Fizz buzz counting up to 100" 
 
