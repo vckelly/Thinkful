6fdaee58b5125246c951b01e7cb5764e41fad01f
Michael Wieher <michael.wieher@gmail.com>
2014-11-13 19:27:19 -0600

    whitespace

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,16 +1,15 @@
 fizz = range(1, 101)
 buzz = []
 
-for i in fizz: 
+for i in fizz:
 
   if not (i % 3) == 0:
 		buzz += "fizz"
 
 	if not (i % 5) == 0:
 		buzz += "buzz"
-  
-		
-print "Fizz buzz counting up to 100" 
+
+print "Fizz buzz counting up to 100"
 
 print buzz or str(i)
 
