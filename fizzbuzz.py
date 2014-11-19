c082ad0fd85379488ff528dfef8fdf9162b9b267
vckelly <vckelly@gmail.com>
2014-11-19 00:47:38 +0000

    Final FB

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
 fizz = range(1, 101)
-buzz = []
+buzz = ""
 
 print "Fizz buzz counting up to 100"
 
 for i in fizz:
 
-    if not (i % 3) == 0:
+    if (i % 3) == 0:
         buzz += "fizz"
 
-    if not (i % 5) == 0:
-        buzz += "buzz"
+    if (i % 5) == 0:
+        buzz += "buzz"    
+    
+    elif not (i % 5) == 0 and not (i % 3) == 0:
+        buzz += str(i)
+        
+    buzz += ", "
 
-    # if <something>
-    # buzz += str(i)
-
-print buzz
+print buzz[:-2]
 
