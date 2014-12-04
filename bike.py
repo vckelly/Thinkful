723f74f106efe7e9444b91f3a921b1177fed046e
vckelly <vckelly@gmail.com>
2014-12-04 01:17:00 +0000

    Last Bike Push

--- bike.py
+++ bike.py
@@ -11,24 +11,30 @@ class BikeModels(Manufacturer):
   def __init__(self, modelname):
     self.modelname = modelname
     
+  def modelcost(self, framecost, wheelcost):
+    self.framecost = framecost
+    self.wheelcost = wheelcost
+    
     numwheels = 2.00
     
     wheelcost = wheelcost * numwheels
       
     
 class BikeShops(Manufacturer):
-  def __init__(self, shopname):
+  def __init__(self, shopname, profitmarg):
     self.shopname = shopname
+    self.profitmarg = profitmarg
+    
+  def modelcost(self, framecost, wheelcost):
+    self.framecost = framecost
+    self.wheelcost = wheelcost
     
   def inventory(self, numinstock):
     self.numinstock = numinstock
     
-  def shoptotalprice(self, shopprofit):
-    self.shopprofit = shopprofit
+  shopprofit = modelcost * profitmarg
     
-    shopprofit = modelcost * shopprofit
-    
-class Customers(BicycleInd):
+class Customers(object):
   def __init__(self, name, budget, market):
     self.name = name
     self.budget = budget
